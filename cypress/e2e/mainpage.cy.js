1ce3a9471525f2dbdcc2132025a9d030e45537c2
Tuomas Rantataro <tuomas.rantataro@gmail.com>
2022-08-24 22:09:30 +0300

    fixed cypress running command

--- cypress/e2e/mainpage.cy.js
+++ cypress/e2e/mainpage.cy.js
@@ -1,6 +1,8 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:5000/', {
+      timeout: 120e3  // first opening seems very slow, setting timeout to max
+    })
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
