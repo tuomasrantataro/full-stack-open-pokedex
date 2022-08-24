ba4bd6b0c5be935bf50747afed533c0625f56c39
Tuomas Rantataro <tuomas.rantataro@gmail.com>
2022-08-24 19:48:25 +0300

    add another test

--- cypress/e2e/mainpage.cy.js
+++ cypress/e2e/mainpage.cy.js
@@ -5,3 +5,10 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+describe('Single pokemon view', function() {
+  it('can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
