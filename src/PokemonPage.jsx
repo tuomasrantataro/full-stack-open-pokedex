f53e45d655bb7305eaa44a1b26335473eba1ab3f
Tuomas Rantataro <tuomas.rantataro@gmail.com>
2022-08-25 23:41:50 +0300

    add commented out logging

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -27,6 +27,7 @@ const PokemonPage = ({ previous, next }) => {
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
 
   //console.log('hiddenAbility:', hiddenAbility)
+  //console.log('normalAbility:', normalAbility)
   return (
     <>
       <div className="links">
