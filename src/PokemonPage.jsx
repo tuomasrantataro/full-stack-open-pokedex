7c3f7c2f405bcf72548122e0b34a8fc54c0f3efc
Tuomas Rantataro <tuomas.rantataro@gmail.com>
2022-08-27 12:51:08 +0300

    commit with no skip

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -27,7 +27,7 @@ const PokemonPage = ({ previous, next }) => {
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
 
   //console.log('hiddenAbility:', hiddenAbility)
-  // test line 11
+  // test line 1
   // test line 2
   return (
     <>
