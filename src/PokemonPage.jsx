fae2251a006d853fe244c9a68e4e07340a03459f
Tuomas Rantataro <tuomas.rantataro@gmail.com>
2022-08-27 12:24:51 +0300

    1st commit

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -28,7 +28,7 @@ const PokemonPage = ({ previous, next }) => {
 
   //console.log('hiddenAbility:', hiddenAbility)
   // test line 11
-  // test line 22
+  // test line 2
   return (
     <>
       <div className="links">
